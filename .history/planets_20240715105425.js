fd081795f8f0895e5672233bc6787dbdc1609538
rmdayberry <dayberryrm@gmail.com>
2024-07-15 11:02:31 -0400

    Create HTML and JS files and completed project tasks
    Create HTML and JS files and completed project tasks for filter array exercise

---
+++ .history/planets_20240715105425.js
@@ -0,0 +1,16 @@
+const planets = [
+  { name: "Mercury", temperature: 440, distance: 0.39 },
+  { name: "Venus", temperature: 737, distance: 0.72 },
+  { name: "Earth", temperature: 288, distance: 1 },
+  { name: "Mars", temperature: 253, distance: 1.5 },
+  { name: "Jupiter", temperature: 163, distance: 5.2 },
+  { name: "Saturn", temperature: 133, distance: 9.58 },
+  { name: "Uranus", temperature: 78, distance: 19.22 },
+  { name: "Neptune", temperature: 73, distance: 30.05 },
+];
+// Task 1: filter out planets in which the temperature is between 253 K and 323 K, inclusive.
+const tempZone = planets.filter(
+  (planet) => planets.temperature >= 253 && planets.temperature <= 323
+);
+
+console.log(tempZone);
---
+++ .history/planets_20240715105702.js
@@ -0,0 +1,18 @@
+const planets = [
+  { name: "Mercury", temperature: 440, distance: 0.39 },
+  { name: "Venus", temperature: 737, distance: 0.72 },
+  { name: "Earth", temperature: 288, distance: 1 },
+  { name: "Mars", temperature: 253, distance: 1.5 },
+  { name: "Jupiter", temperature: 163, distance: 5.2 },
+  { name: "Saturn", temperature: 133, distance: 9.58 },
+  { name: "Uranus", temperature: 78, distance: 19.22 },
+  { name: "Neptune", temperature: 73, distance: 30.05 },
+];
+// Task 1: filter out planets in which the temperature is between 253 K and 323 K, inclusive.
+const tempZone = planets.filter(
+  (planet) => planet.temperature >= 253 && planet.temperature <= 323
+);
+
+console.log(tempZone);
+
+//Task 2: Filter out planets in which the distance from the Sun is between 0.75 and 1.5 AU, inclusive.
